175c293f712f9ec38e071e9e5dbe80116c267656
zhouchaowei <zhouchaowei96492@163.com>
2022-11-27 14:43:53 +0800

    添加测试类/添加mybatis配置文件

--- task-platform-controller/src/test/java/com/newcore/task/platform/controller/PageRedirectControllerTest.java
+++ task-platform-controller/src/test/java/com/newcore/task/platform/controller/PageRedirectControllerTest.java
@@ -1,8 +1,11 @@
 package com.newcore.task.platform.controller;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 /**
  * 页面重定向控制器测试类
  * @author zhouchaowei
  */
+@SpringBootTest
 public class PageRedirectControllerTest {
 }
